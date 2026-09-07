52507303aeded70536a31292a7aba410c6bc846d
AhmadHaider-stu <ahmadhaider24504@gmail.com>
2026-09-07 19:50:20 +0300

    fixed the google redirect

--- app/auth/google.py
+++ app/auth/google.py
@@ -9,7 +9,7 @@ oauth.register(
     client_secret = CLIENT_SECRET,
     client_kwargs={
         'scope' : 'email openid profile',
-        'redirect_uri':'https://salon-project-bc9b8cfc.fastapicloud.dev/login/auth'
+        'redirect_uri':'https://aymanmatarsalon-production.up.railway.app/login/auth'
     }
 )
 
