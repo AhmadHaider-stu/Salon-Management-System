6dc5eb9ed4d7c6f28ceb77dd9e9a76eab67da48d
AhmadHaider-stu <ahmadhaider24504@gmail.com>
2026-09-07 18:55:20 +0300

    fixed the port issue

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
+import os
 import uvicorn
 from app.main import app
 
 if __name__ == "__main__":
     uvicorn.run(
         app=app,
-        host="localhost",
-        port=8000,
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 8000)),
         # reload=True
     )
