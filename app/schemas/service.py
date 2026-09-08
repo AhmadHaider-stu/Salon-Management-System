658298992eeed41bbcc78314bf0a003599b21edf
AhmadHaider-stu <ahmadhaider24504@gmail.com>
2026-09-08 14:48:51 +0300

    service pricing

--- app/schemas/service.py
+++ app/schemas/service.py
@@ -6,14 +6,14 @@ from typing import Optional
 class ServiceCreate(BaseModel):
     category : ServiceCategory
     description : str = Field( max_length=30)
-    price : float = Field(ge=10 ,le=500)
-    time_duration : int = Field(ge=10,le=180)
+    price : float = Field(ge=1 ,le=500)
+    time_duration : int = Field(ge=1,le=180)
 
 class ServiceUpdate(BaseModel):
     category : Optional[ServiceCategory] = None
     description : Optional[str] = Field(default=None, max_length=30)
-    price : Optional[float] = Field(default=None,ge=10 ,le=500)
-    time_duration : Optional[int] = Field(default=None,ge=10,le=180)
+    price : Optional[float] = Field(default=None,ge=1 ,le=500)
+    time_duration : Optional[int] = Field(default=None,ge=1,le=180)
     active : Optional[bool] = None
 
 class ServiceResponse(BaseModel):
